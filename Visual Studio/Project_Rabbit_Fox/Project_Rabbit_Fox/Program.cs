124ea3892d44b7c996901beb9a1cae99b26d65c2
m111balazs <martonbalazs0101@gmail.com>
2024-09-20 22:16:28 +0200

    Project generated

---
+++ Visual Studio/Project_Rabbit_Fox/Project_Rabbit_Fox/Program.cs
@@ -0,0 +1,8 @@
+﻿
+WriteInTheMiddle("Welcome! To start the simulation press any key");
+
+void WriteInTheMiddle(string outputText)
+{
+    Console.SetCursorPosition((Console.WindowWidth - outputText.Length) / 2, Console.CursorTop);
+    Console.WriteLine(outputText);
+}
