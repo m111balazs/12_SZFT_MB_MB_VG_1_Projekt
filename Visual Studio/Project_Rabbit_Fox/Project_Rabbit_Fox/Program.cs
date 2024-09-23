d10337088820c772ad434370328f1865148f896e
SergeySandwich <vorosgergo06@gmail.com>
2024-09-23 17:17:19 +0200

    ujrakezdes

--- Visual Studio/Project_Rabbit_Fox/Project_Rabbit_Fox/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-WriteInTheMiddle("Welcome! To start the simulation press any key");
-
-void WriteInTheMiddle(string outputText)
-{
-    Console.SetCursorPosition((Console.WindowWidth - outputText.Length) / 2, Console.CursorTop);
-    Console.WriteLine(outputText);
-}
