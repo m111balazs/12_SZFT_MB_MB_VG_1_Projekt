db38893927c79c792d5447299bed5e7c480e2a39
SergeySandwich <vorosgergo06@gmail.com>
2024-09-30 16:31:46 +0200

    title kiiratas

--- The game of life/Program.cs
+++ The game of life/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿TitleDisplay("The Game of Life")
+
+
+
+
+void TitleDisplay(string title)
+{
+    Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+
+    Console.WriteLine(title);
+
+    Console.SetCursorPosition(0, Console.CursorTop)
+}
